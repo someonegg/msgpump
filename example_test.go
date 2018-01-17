72f6fba6804dba3519461fbfa16050fc0de7243d
kngin <someonegg@163.com>
2018-01-17 10:33:29 +0800

    update doc

--- example_test.go
+++ example_test.go
@@ -18,13 +18,13 @@ import (
 const TheAddr = "127.0.0.1:7000"
 const WriteQueueSize = 100
 
-type Client struct {
+type ClientPeer struct {
 	pump *msgpump.Pump
 
 	helloD syncx.DoneChan
 }
 
-func (c *Client) Start() {
+func (p *ClientPeer) Start() {
 	conn, err := net.Dial("tcp", TheAddr)
 	if err != nil {
 		log.Fatal(err)
@@ -32,128 +32,128 @@ func (c *Client) Start() {
 
 	mrw := msgpump.NetconnMRW(conn)
 
-	c.pump = msgpump.NewPump(mrw, c, WriteQueueSize)
-	c.pump.Start(nil)
+	p.pump = msgpump.NewPump(mrw, p, WriteQueueSize)
+	p.pump.Start(nil)
 }
 
-func (c *Client) Hello(m msgpump.Message) {
-	c.pump.Output("client-hello", m)
+func (p *ClientPeer) Hello(m msgpump.Message) {
+	p.pump.Output("client-hello", m)
 }
 
-func (c *Client) Ask(m msgpump.Message) {
-	c.pump.Output("client-ask", m)
+func (p *ClientPeer) Ask(m msgpump.Message) {
+	p.pump.Output("client-ask", m)
 }
 
-func (c *Client) Bye(m msgpump.Message) {
-	c.pump.Output("client-bye", m)
+func (p *ClientPeer) Bye(m msgpump.Message) {
+	p.pump.Output("client-bye", m)
 }
 
-func (c *Client) Process(ctx context.Context, t string, m msgpump.Message) {
+func (p *ClientPeer) Process(ctx context.Context, t string, m msgpump.Message) {
 	log.Printf("client receive message: %v, %v", t, string(m))
 
 	switch t {
 	case "server-hello":
-		c.OnServerHello(m)
+		p.OnServerHello(m)
 	case "server-answer":
-		c.OnServerAnswer(m)
+		p.OnServerAnswer(m)
 	case "server-bye":
-		c.OnServerBye(m)
+		p.OnServerBye(m)
 	default:
 		log.Print("unknown server message")
 	}
 }
 
-func (c *Client) OnServerHello(m msgpump.Message) {
-	c.helloD.SetDone()
+func (p *ClientPeer) OnServerHello(m msgpump.Message) {
+	p.helloD.SetDone()
 }
 
-func (c *Client) OnServerAnswer(m msgpump.Message) {
+func (p *ClientPeer) OnServerAnswer(m msgpump.Message) {
 }
 
-func (c *Client) OnServerBye(m msgpump.Message) {
-	c.pump.Stop()
+func (p *ClientPeer) OnServerBye(m msgpump.Message) {
+	p.pump.Stop()
 }
 
-func (c *Client) WaitStop() {
+func (p *ClientPeer) WaitStop() {
 	select {
-	case <-c.pump.StopD():
+	case <-p.pump.StopD():
 	}
 }
 
 func client() {
-	c := &Client{helloD: syncx.NewDoneChan()}
+	p := &ClientPeer{helloD: syncx.NewDoneChan()}
 
-	c.Start()
+	p.Start()
 
-	c.Hello([]byte("aaa"))
+	p.Hello([]byte("aaa"))
 
-	<-c.helloD
+	<-p.helloD
 
 	for i := 0; i < 3; i++ {
-		c.Ask([]byte(fmt.Sprint("bbb", i)))
+		p.Ask([]byte(fmt.Sprint("bbb", i)))
 		time.Sleep(time.Millisecond)
 	}
 
-	c.Bye([]byte("ccc"))
+	p.Bye([]byte("ccc"))
 
-	c.WaitStop()
+	p.WaitStop()
 
-	log.Printf("client stop, error: %v", c.pump.Error())
+	log.Printf("client stop, error: %v", p.pump.Error())
 }
 
-type SClient struct {
+type ServerPeer struct {
 	pump *msgpump.Pump
 }
 
-func (c *SClient) Start(conn net.Conn) {
+func (p *ServerPeer) Start(conn net.Conn) {
 	mrw := msgpump.NetconnMRW(conn)
 
-	c.pump = msgpump.NewPump(mrw, c, WriteQueueSize)
-	c.pump.Start(nil)
+	p.pump = msgpump.NewPump(mrw, p, WriteQueueSize)
+	p.pump.Start(nil)
 }
 
-func (c *SClient) Hello(m msgpump.Message) {
-	c.pump.Output("server-hello", m)
+func (p *ServerPeer) Hello(m msgpump.Message) {
+	p.pump.Output("server-hello", m)
 }
 
-func (c *SClient) Answer(m msgpump.Message) {
-	c.pump.Output("server-answer", m)
+func (p *ServerPeer) Answer(m msgpump.Message) {
+	p.pump.Output("server-answer", m)
 }
 
-func (c *SClient) Bye(m msgpump.Message) {
-	c.pump.Output("server-bye", m)
+func (p *ServerPeer) Bye(m msgpump.Message) {
+	p.pump.Output("server-bye", m)
 }
 
-func (c *SClient) Process(ctx context.Context, t string, m msgpump.Message) {
+func (p *ServerPeer) Process(ctx context.Context, t string, m msgpump.Message) {
 	log.Printf("server receive message: %v, %v", t, string(m))
 
 	switch t {
 	case "client-hello":
-		c.OnClientHello(m)
+		p.OnClientHello(m)
 	case "client-ask":
-		c.OnClientAsk(m)
+		p.OnClientAsk(m)
 	case "client-bye":
-		c.OnClientBye(m)
+		p.OnClientBye(m)
 	default:
 		log.Print("unknown client message")
 	}
 }
 
-func (c *SClient) OnClientHello(m msgpump.Message) {
-	c.Hello(m)
+func (p *ServerPeer) OnClientHello(m msgpump.Message) {
+	p.Hello(m)
 }
 
-func (c *SClient) OnClientAsk(m msgpump.Message) {
-	c.Answer(m)
+func (p *ServerPeer) OnClientAsk(m msgpump.Message) {
+	p.Answer(m)
 }
 
-func (c *SClient) OnClientBye(m msgpump.Message) {
-	c.Bye(m)
+func (p *ServerPeer) OnClientBye(m msgpump.Message) {
+	p.Bye(m)
 }
 
-func (c *SClient) WaitStop() {
+func (p *ServerPeer) WaitStop() {
 	select {
-	case <-c.pump.StopD():
+	case <-p.pump.StopD():
 	}
 }
 
@@ -172,11 +172,11 @@ func server(listenD syncx.DoneChan) {
 			log.Fatal(err)
 		}
 
-		c := &SClient{}
-		c.Start(conn)
+		p := &ServerPeer{}
+		p.Start(conn)
 		go func() {
-			c.WaitStop()
-			log.Printf("sclient stop, error: %v", c.pump.Error())
+			p.WaitStop()
+			log.Printf("sclient stop, error: %v", p.pump.Error())
 		}()
 	}
 }
